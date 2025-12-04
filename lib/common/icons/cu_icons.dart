bb8c43aa1694644663e5253221ec5dfae8e1a94e
sergioc12r <sergio951112@gmail.com>
2025-12-04 11:17:33 -0500

    [Icons] - Add icons

---
+++ lib/common/icons/cu_icons.dart
@@ -0,0 +1,6 @@
+/// Customer Icons Class
+class CUIcons {
+  static String linkedin = "assets/icons/linked_in_svg.svg";
+  static String github = "assets/icons/github_svg.svg";
+  static String instagram = "assets/icons/instagram_svg.svg";
+}
