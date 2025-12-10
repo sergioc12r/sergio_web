b0deb552d49c430cf6f1eb3862b96c1951c74ce9
sergioc12r <sergio951112@gmail.com>
2025-12-10 10:25:17 -0500

    [Footer] - add ln labels in footer location

--- lib/common/footer/ui/footer_location.dart
+++ lib/common/footer/ui/footer_location.dart
@@ -1,16 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class FooterLocation extends StatelessWidget {
   const FooterLocation({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final strings = AppLocalizations.of(context)!;
+    final style = Theme.of(context).textTheme;
     return Row(
       crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 15,
       children: [
         Icon(Icons.location_on_outlined),
-        const SizedBox(width: 15),
-        Text('Bogota, Colombia')
+        Text(strings.footer_location, style: style.bodyMedium)
       ],
     );
   }
