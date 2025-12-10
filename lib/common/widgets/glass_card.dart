3064feab504a1eac0441178573b6c00b60135b5a
sergioc12r <sergio951112@gmail.com>
2025-12-10 11:15:58 -0500

    [Common] - Changes in GlassCard

--- lib/common/widgets/glass_card.dart
+++ lib/common/widgets/glass_card.dart
@@ -4,52 +4,45 @@ import 'package:flutter/material.dart';
 class GlassCard extends StatelessWidget {
   final Widget child;
   final double blurAmount;
-  final double elevation;
-  final Color overlayColor;
 
   const GlassCard({
     super.key,
     required this.child,
-    this.blurAmount = 20.0,
-    this.elevation = 4.0,
-    this.overlayColor = const Color.fromRGBO(255, 255, 255, 0.2), // Ligeramente más opaco
+    this.blurAmount = 5.0,
   });
 
   @override
   Widget build(BuildContext context) {
-    // Si necesitas BoxShadow, la colocamos aquí.
-    const double shadowBlurRadius = 15.0;
+    final styles = Theme.of(context).cardTheme;
+    const double radius = 25.0;
 
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(20.0),
-      child: BackdropFilter(
-        filter: ImageFilter.blur(
-          sigmaX: blurAmount,
-          sigmaY: blurAmount,
-        ),
-        child: Container(
-          decoration: BoxDecoration(
-            color: overlayColor,
-            borderRadius: BorderRadius.circular(20.0),
-            border: Border.all(
-              color: Colors.white.withOpacity(0.4),
-              width: 1.0,
-            ),
-
-            boxShadow: [
-              BoxShadow(
-                color: Colors.black.withOpacity(0.1),
-                blurRadius: shadowBlurRadius,
-                offset: const Offset(0, 5),
-              ),
-              BoxShadow(
-                color: Colors.white.withOpacity(0.15),
-                blurRadius: 5.0,
-                spreadRadius: -2.0,
-              ),
-            ],
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(radius),
+        boxShadow: [
+          BoxShadow(
+            color: styles.shadowColor!.withOpacity(0.2),
+            spreadRadius: 0.5,
+            blurRadius: 10,
+            offset: const Offset(0, 4),
           ),
-          child: child,
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(radius),
+        child: BackdropFilter(
+          filter: ImageFilter.blur(sigmaX: blurAmount, sigmaY: blurAmount),
+          child: Container(
+            decoration: BoxDecoration(
+              color: styles.color!.withOpacity(0.5),
+              borderRadius: BorderRadius.circular(radius),
+              border: Border.all(
+                color: styles.shadowColor!.withOpacity(0.9),
+                width: 1.2,
+              ),
+            ),
+            child: child,
+          ),
         ),
       ),
     );
