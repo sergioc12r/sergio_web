79f7937a95cecd7f916529425eb10e10223b58a1
sergioc12r <sergio951112@gmail.com>
2025-12-10 10:18:29 -0500

    [Footer] - add ln labels in contact us

--- lib/common/footer/ui/footer_contact_us.dart
+++ lib/common/footer/ui/footer_contact_us.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 class FooterContactUs extends StatelessWidget {
   const FooterContactUs({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final strings = AppLocalizations.of(context)!;
+    final styles = Theme.of(context).textTheme;
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 3,
       children: [
-        Text('Contact me:'),
+        Text(strings.footer_contact_label, style: styles.bodySmall),
+
         /// TODO add launch url with email curl
-        Text('sergio951112@gmail.com')
+        Text(strings.email, style: styles.bodyMedium)
       ],
     );
   }
