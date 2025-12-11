17362aeba2ca84a60b56b0d5c4abe0d8db9578d7
sergioc12r <sergio951112@gmail.com>
2025-12-11 17:37:32 -0500

    [Contact] - refactor message form

--- lib/contact/ui/send_me_message_form.dart
+++ lib/contact/ui/send_me_message_form.dart
@@ -1,10 +1,182 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:sergio_web/common/widgets/cu_gradient_button.dart';
+import 'package:sergio_web/common/widgets/glass_card.dart';
 
-class SendMeMessageForm extends StatelessWidget {
+class SendMeMessageForm extends StatefulWidget {
   const SendMeMessageForm({super.key});
 
   @override
+  State<SendMeMessageForm> createState() => _SendMeMessageFormState();
+}
+
+class _SendMeMessageFormState extends State<SendMeMessageForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  /// Controllers
+  final TextEditingController _nameController = TextEditingController();
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _subjectController = TextEditingController();
+  final TextEditingController _messageController = TextEditingController();
+
+  /// Simulate Sending Message
+  void _submitForm() {
+    if (_formKey.currentState!.validate()) {
+      ///TODO complete send message and show snackbar
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(
+              'Mensaje Enviado:\nNombre: ${_nameController.text}, Email: ${_emailController.text}'),
+        ),
+      );
+
+      _nameController.clear();
+      _emailController.clear();
+      _subjectController.clear();
+      _messageController.clear();
+    }
+  }
+
+  @override
+  void dispose() {
+    _nameController.dispose();
+    _emailController.dispose();
+    _subjectController.dispose();
+    _messageController.dispose();
+    super.dispose();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final textStyle = Theme.of(context).textTheme;
+    final fieldStyle = Theme.of(context).inputDecorationTheme;
+    final strings = AppLocalizations.of(context)!;
+
+    return GlassCard(
+      child: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: LayoutBuilder(builder: (context, constraints) {
+          return SizedBox(
+            width: constraints.maxWidth,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Text(
+                    strings.contact_form_title,
+                    style: textStyle.headlineMedium,
+                  ),
+                  const SizedBox(height: 24),
+                  Row(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      Expanded(
+                        child: _buildFormField(
+                          fieldStyle: fieldStyle,
+                          textStyle: textStyle,
+                          label: strings.contact_form_name_label,
+                          hint: strings.contact_form_name_place_holder,
+                          controller: _nameController,
+                          validator: (value) => _validateName(value, strings),
+                        ),
+                      ),
+                      const SizedBox(width: 16),
+                      Expanded(
+                        child: _buildFormField(
+                          fieldStyle: fieldStyle,
+                          textStyle: textStyle,
+                          label: strings.contact_form_email_label,
+                          hint: strings.contact_form_email_place_holder,
+                          controller: _emailController,
+                          keyboardType: TextInputType.emailAddress,
+                          validator: (value) => _validateEmail(value, strings),
+                        ),
+                      ),
+                    ],
+                  ),
+                  const SizedBox(height: 16),
+                  _buildFormField(
+                    fieldStyle: fieldStyle,
+                    textStyle: textStyle,
+                    label: strings.contact_form_subject_label,
+                    hint: strings.contact_form_subject_place_holder,
+                    controller: _subjectController,
+                    validator: (value) => _validateRequired(value, strings),
+                  ),
+                  const SizedBox(height: 16),
+                  _buildFormField(
+                    textStyle: textStyle,
+                    fieldStyle: fieldStyle,
+                    label: strings.contact_form_subject_body_label,
+                    hint: strings.contact_form_subject_body_place_holder,
+                    controller: _messageController,
+                    maxLines: 6,
+                    validator: (value) => _validateRequired(value, strings),
+                  ),
+                  const SizedBox(height: 24),
+                  CuGradientButton(callback: _submitForm, title: strings.contact_form_send_button_label),
+                ],
+              ),
+            ),
+          );
+        }),
+      ),
+    );
+  }
+
+  Widget _buildFormField({
+    required TextTheme textStyle,
+    required InputDecorationTheme fieldStyle,
+    required String label,
+    required String hint,
+    required TextEditingController controller,
+    required String? Function(String?) validator,
+    int maxLines = 1,
+    TextInputType keyboardType = TextInputType.text,
+  }) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: textStyle.labelMedium,
+        ),
+        const SizedBox(height: 8),
+        TextFormField(
+          controller: controller,
+          keyboardType: keyboardType,
+          maxLines: maxLines,
+          style: textStyle.bodyMedium,
+          validator: validator,
+          decoration: InputDecoration(
+            hintText: hint,
+            hintStyle: fieldStyle.hintStyle,
+          ),
+        ),
+      ],
+    );
+  }
+
+  String? _validateRequired(String? value, AppLocalizations strings) {
+    if (value == null || value.isEmpty) {
+      return strings.contact_validator;
+    }
+    return null;
+  }
+
+  String? _validateEmail(String? value, AppLocalizations strings) {
+    if (value == null || value.isEmpty) {
+      return strings.contact_email_empty_validator;
+    }
+    final emailRegex = RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$');
+    if (!emailRegex.hasMatch(value)) {
+      return strings.contact_email_validator;
+    }
+    return null;
+  }
+
+  String? _validateName(String? value, AppLocalizations strings) {
+    return _validateRequired(value, strings);
   }
 }
