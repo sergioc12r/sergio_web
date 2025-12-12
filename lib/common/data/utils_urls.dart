c09d221aa85f20e6b032b6bc16218fd3604ee096
sergioc12r <sergio951112@gmail.com>
2025-12-12 15:14:55 -0500

    [Profile] - Add profile image

--- lib/common/data/utils_urls.dart
+++ lib/common/data/utils_urls.dart
@@ -2,10 +2,14 @@ class UtilsUrls {
   static const _instagram = "https://www.instagram.com/serch_c_/";
   static const _github = "https://github.com/sergioc12r";
   static const _linkedin = "https://www.linkedin.com/in/sergiocarorojas95/";
+  static const _profileImage =
+      "https://raw.githubusercontent.com/sergioc12r/assets/main/profile.jpg";
 
   static String get instagram => _instagram;
 
   static String get github => _github;
 
   static String get linkedin => _linkedin;
+
+  static String get profileImage => _profileImage;
 }
