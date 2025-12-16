bc0c5f7653f2cf809fd743389037b74a2f3d4104
sergioc12r <sergio951112@gmail.com>
2025-12-15 21:10:08 -0500

    [Common] - Add animation size change

--- lib/common/widgets/glass_card.dart
+++ lib/common/widgets/glass_card.dart
@@ -1,47 +1,80 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 
-class GlassCard extends StatelessWidget {
+class GlassCard extends StatefulWidget {
   final Widget child;
   final double blurAmount;
+
+  /// Animation properties
+  final double defaultScale;
+  final double hoverScale;
+  final Duration animationDuration;
 
   const GlassCard({
     super.key,
     required this.child,
-    this.blurAmount = 5.0,
+    this.blurAmount = 0.3,
+    this.defaultScale = 1.0,
+    this.hoverScale = 1.005,
+    this.animationDuration = const Duration(milliseconds: 200),
   });
+
+  @override
+  State<GlassCard> createState() => _GlassCardState();
+}
+
+class _GlassCardState extends State<GlassCard> {
+  bool _isHovering = false;
+  static const double _radius = 25.0;
+
+  void _onHover(bool isHovering) {
+    setState(() {
+      _isHovering = isHovering;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     final styles = Theme.of(context).cardTheme;
-    const double radius = 25.0;
 
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(radius),
-        boxShadow: [
-          BoxShadow(
-            color: styles.shadowColor!.withOpacity(0.2),
-            spreadRadius: 0.5,
-            blurRadius: 10,
-            offset: const Offset(0, 4),
-          ),
-        ],
-      ),
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(radius),
-        child: BackdropFilter(
-          filter: ImageFilter.blur(sigmaX: blurAmount, sigmaY: blurAmount),
-          child: Container(
-            decoration: BoxDecoration(
-              color: styles.color!.withOpacity(0.5),
-              borderRadius: BorderRadius.circular(radius),
-              border: Border.all(
-                color: styles.shadowColor!.withOpacity(0.9),
-                width: 1.2,
-              ),
+    /// Calculate current scale
+    final double scale = _isHovering ? widget.hoverScale : widget.defaultScale;
+
+    return MouseRegion(
+      onEnter: (_) => _onHover(true),
+      onExit: (_) => _onHover(false),
+      child: AnimatedContainer(
+        duration: widget.animationDuration,
+        curve: Curves.easeInOut,
+        transformAlignment: Alignment.center,
+        transform: Matrix4.identity()..scale(scale, scale),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(_radius),
+          boxShadow: [
+            BoxShadow(
+              color: styles.shadowColor!.withOpacity(_isHovering ? 0.4 : 0.2),
+              spreadRadius: 0.5,
+              blurRadius: _isHovering ? 15 : 10,
+              offset: Offset(0, _isHovering ? 6 : 4),
             ),
-            child: child,
+          ],
+        ),
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(_radius),
+          child: BackdropFilter(
+            filter: ImageFilter.blur(
+                sigmaX: widget.blurAmount, sigmaY: widget.blurAmount),
+            child: Container(
+              decoration: BoxDecoration(
+                color: styles.color!.withOpacity(0.5),
+                borderRadius: BorderRadius.circular(_radius),
+                border: Border.all(
+                  color: styles.shadowColor!.withOpacity(0.9),
+                  width: 1.2,
+                ),
+              ),
+              child: widget.child,
+            ),
           ),
         ),
       ),
